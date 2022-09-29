38d46121edd72383b5e80c712a92e189c267b1ae
Chad Morelli <chadmorelli@outlook.com>
2022-09-29 01:43:31 -0500

    Update SQL Chicken Sandwich Scripts.sql

--- SQL Chicken Sandwich Scripts.sql
+++ SQL Chicken Sandwich Scripts.sql
@@ -2,7 +2,8 @@
 THE GREAT CHICKEN SANDWICH SHOWDOWN OF 2022
 Five dudes rate every fast food chicken sandwich in a six mile radius
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
+Creating Views, Converting Data Types
 
 */
 
@@ -16,7 +17,7 @@ Select *
 From ChickenSand..SandwichList
 Where SandwichName like '%chicken%'
 
--- WHICH CHICKEN SANDWICH OFFERS THE BEST VALUE BASED ON CALORIES TO PRICE RATION?
+-- 1. WHICH CHICKEN SANDWICH OFFERS THE BEST VALUE BASED ON CALORIES TO PRICE RATION?
 Select SandwichName, Round ((SandwichPrice / SandwichCalories), 4) as PricePerCalorie, SandwichPrice, SandwichCalories
 From ChickenSand..SandwichList
 Where SandwichName like '%chicken%'
@@ -24,7 +25,7 @@ Order By PricePerCalorie
 
 
 
--- TOTAL PRICE IF I BUY ONE REGULAR AND ONE SPICY SANDWICH AT EACH RESTAURANT
+-- 2. TOTAL PRICE IF I BUY ONE REGULAR AND ONE SPICY SANDWICH AT EACH RESTAURANT
 Select RestaurantName, (SUM(SandwichPrice)) as TotalCost
 From ChickenSand..SandwichList
 Where SandwichName like '%chicken%' 
@@ -32,7 +33,7 @@ GROUP BY RestaurantName
 Order By TotalCost
 
 
--- WHICH CHICKEN SANDWICH COSTS THE LEAST BASED ON PRICE AND TRAVEL? 
+-- 3. WHICH CHICKEN SANDWICH COSTS THE LEAST BASED ON PRICE AND TRAVEL? 
 -- ASSUMES A COST OF 62 CENTS PER MILE
 Select SandwichName
 , (Distance * 0.62 + SandwichPrice) as TotalCost
@@ -43,7 +44,7 @@ Where SandwichName like '%chicken%'
 Order By TotalCost 
 
 
--- HOW MUCH DID I SPEND ON PURCHASING 24 TOTAL SANDWICHES FOR THE TASTE TEST, PLUS SALES TAX? (WITH CAST, $ FORMATTING)
+-- 4. HOW MUCH DID I SPEND ON PURCHASING 24 TOTAL SANDWICHES FOR THE TASTE TEST, PLUS SALES TAX? (WITH CAST, $ FORMATTING)
 -- *** I AM NOT PROUD OF THIS NUMBER ***
 Select Count(SandwichName) * 2 as TotalSandwiches
 , '$'+convert(varchar,cast(sum(SandwichPrice * 2) as money)) as CostofSandwiches
@@ -53,7 +54,7 @@ From ChickenSand..SandwichList
 Where SandwichName like '%chicken%'
 
 
--- CHICKEN SANDWICH SCORES DISPLAYED WITH PRICES
+-- 5. CHICKEN SANDWICH SCORES DISPLAYED WITH PRICES
 Select rat.SandwichName
 , '$'+convert(varchar,cast(lis.SandwichPrice as money)) as SandwichPrice
 , rat.Dane, rat.Logan, rat.Skyler, rat.Vinnie, rat.Chad
@@ -64,10 +65,12 @@ Where rat.SandwichName like '%chicken%'
 Order By rat.SandwichName
 
 
--- AVERAGE SCORE FOR EACH CHICKEN SANDWICH
-Select rat.SandwichName, (rat.Dane+rat.Logan+rat.Skyler+rat.Vinnie+rat.Chad)/5 as AvgScore, '$'+convert(varchar,cast(lis.SandwichPrice as money)) as SandwichPrice
-FROM SandwichRatings rat
+-- 6. AVERAGE SCORE FOR EACH CHICKEN SANDWICH
+Select rat.SandwichName, (rat.Dane+rat.Logan+rat.Skyler+rat.Vinnie+rat.Chad)/5 as AvgScore
+, '$'+convert(varchar,cast(lis.SandwichPrice as money)) as SandwichPrice
+FROM chickenSand..SandwichRatings rat
 Join ChickenSand..SandwichList lis
 On lis.SandwichID = rat.SandwichID
 Where lis.SandwichName like '%chicken%'
 Order By AvgScore desc
+
